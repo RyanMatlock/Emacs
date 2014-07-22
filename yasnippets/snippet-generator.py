4dfc46e1794e2ee889a5c2f46e639f3c98a2a1de
Ryan Matlock <ryan.matlock@gmail.com>
2014-07-22 12:06:24 -0700

    almost ready to test

--- yasnippets/snippet-generator.py
+++ yasnippets/snippet-generator.py
@@ -12,7 +12,12 @@ the target files will be overwritten.
 * TODO features:
 - [ ] use the argparse module (https://docs.python.org/3.4/library/argparse.html)
   to allow arbitrary comment and split characters
+- [ ] allow for extra fields in the snippet so you can set stuff like expand
+  environment and stuff like that (see
+  http://capitaomorte.github.io/yasnippet/snippet-development.html#sec-2-5)
+  (in order to do this, you'll want to check the length of your split string)
 """
+
 import re
 import sys
 import os
@@ -23,7 +28,7 @@ SPLIT_STR = ";;"
 NAME_PREFIX = "# name: "
 KEY_PREFIX = "# key: "
 CONTRIBUTOR_PREFIX = "# contributor: "
-CONTRIBUTOR = "Ryan Matlock <ryan.matlock@gmail.com>"
+contributor = "Ryan Matlock <ryan.matlock@gmail.com>"
 SEPARATOR = "# --"
 
 snippet_defs = []
@@ -31,15 +36,24 @@ while True:
     if len(sys.argv) > 1:
         source = sys.argv[1]
     else:
-        while True:
-            source = input("Enter filename to process: ")
-            if source:
-                break
+        source = input("Enter filename to process (or nothing to exit): ")
+        if source:
+            pass
+        else:
+            exit()
+            
     try:
         source = os.path.expanduser(source)
         with open(source) as f:
             for line in f:
-                snippet_defs.append(line)
+                if line:
+                    if line[0] == COMMENT_CHAR:
+                        pass
+                    else:
+                        # I want to explicity add newline characters
+                        if line[-1] == "\n":
+                            line = line[:-1]
+                        snippet_defs.append(line)
         break
     except IOError as e:
         print("{}".format(e))
@@ -50,51 +64,35 @@ while True:
             exit()
 
 
+source_path = os.path.dirname(source)
+source_base_name, source_ext = os.path.splitext(os.path.basename(source))
+
+target_path = os.path.join(source_path, source_base_name)
+
+try:
+    if not os.path.isdir(target_path):
+        os.mkdir(target_path)
+except OSError as e:
+    print("{}".format(e))
+    print("Unrecoverable error; terminating.")
 
 
-
-snippet_templates = []
-with open(raw_snippet_templates) as raw_snippets:
-    for line in raw_snippets:
-        if line[0] != COMMENT_CHAR:
-            snippet_templates.append(line.split(SPLIT_STR))
-        
-
-OUTPUT_SUBFOLDER = "generated/"
-LATEX_OUTPUT_FILE = "add_to_latex.tex"
-
-# this should overwrite old versions of the output file
-# note that "a" = append, "w" = (over)write, "r" = read
-with open(LATEX_OUTPUT_FILE, "w"):
-    pass
-
-for snippet_template in snippet_templates:
-    name = snippet_template[0]
-    key = snippet_template[1]
-    snippet = snippet_template[2]
-
-    # I'd prefer to explicity add a newline when I want it, so I'll get rid of
-    # it if it's there                      
-    if snippet[-1] == "\n":
-        snippet = snippet[:-1]
-
-
-out_dir = os.path.dirname(fname)
-out_fname, ext = os.path.splitext(os.path.basename(fname))
-out_fname += "-allman" + ext
-
-with open(os.path.join(out_dir, out_fname), "w") as out:
-    for line in lines:
-        out.write(line)
-
-with open(os.path.join(OUTPUT_SUBFOLDER + name), "w") as snippet_file:
-    snippet_file.write(NAME_PREFIX + name + "\n")
-    snippet_file.write(KEY_PREFIX + key + "\n")
-    snippet_file.write(CONTRIBUTOR_PREFIX + CONTRIBUTOR + "\n")
-    snippet_file.write(SEPARATOR + "\n")
-    snippet_file.write(snippet)
-with open(LATEX_OUTPUT_FILE, "a") as latex_file:
-    latex_file.write("%% " + key + ": " + snippet + "\n")
+for snippet_def in snippet_defs:
+    try:
+        name, key, snippet = snippet_def.split[SPLIT_STR]
+        try:
+            with open(os.path.join(target_path, name), "w") as out:
+                out.write(NAME_PREFIX + name + "\n")
+                out.write(KEY_PREFIX + key + "\n")
+                out.write(CONTRIBUTOR_PREFIX + contributor + "\n")
+                out.write(SEPARATOR + "\n")
+                out.write(snippet + "\n")
+        except OSError as e:
+            print("{}".format(e))
+            print("Writing snippet '{}' failed; skipping".format(name))
+    except (ValueError, IndexError) as e:
+        print("{}".format(e))
+        print("Line '{}' incorrectly formatted; skipping.".format(snippet_def))
 
 print("Snippet writing finished!")
 
